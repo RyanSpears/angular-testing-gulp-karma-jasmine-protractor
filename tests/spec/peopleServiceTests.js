aae3b4b4e786c22f0344ef95833dcb92b17ba9b0
Ryan Spears <sperj001@hotmail.com>
2015-01-28 15:41:11 +1030

    Added simple SpecRunner.html for basic Jasmine user testing

--- tests/spec/peopleServiceTests.js
+++ tests/spec/peopleServiceTests.js
@@ -10,8 +10,15 @@ describe("The peopleService ", function() {
 
     it('should return three expense items', inject(function(peopleService){
         expect(peopleService.getPeople().length).toBe(2);
-    }))
+    }));
 
+    it('should return a taxi person called Ryan Spears', inject(function(peopleService) {
+        var people = peopleService.getPeople();
+        var person = { Id: 1, Name: 'Ryan Spears' };
+        expect(people).toContain(person);
+    }));
+
+    // example of s apy
     it('should be able to all getPeople function', inject(function (peopleService) {
         var spy = spyOn(peopleService, 'getPeople');
         peopleService.getPeople();
